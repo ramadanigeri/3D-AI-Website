3e9aab574b3525f549647f4c9e6b183b558f40c9
Geri Ramadani <gramadan@estee.com>
2023-04-22 21:08:31 +0200

    Fix full image upload + linting

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useSnapshot } from 'valtio';
-
-import config from '../config/config';
 import state from '../store';
-import { download } from '../assets';
 import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes, DownloadTab } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
